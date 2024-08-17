6edb142ebd7424e4f4a7d8d4c8f7eb1c193a7984
T01764 <pedro.rodrigues@taking.com.br>
2024-08-17 17:07:09 -0300

    feat: fix code

--- audit_app/admin.py
+++ audit_app/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
+from rangefilter.filters import (
+    DateRangeQuickSelectListFilterBuilder,
+)
 
 from audit_app.models import Audit
 
@@ -6,7 +9,10 @@ from audit_app.models import Audit
 # Register your models here.
 class AuditAdmin(admin.ModelAdmin):
     model = Audit
-    list_display = ('id','audited_user', 'description', 'creation_date')
-    search_fields =
+    list_display = ('id', 'audited_user', 'description', 'creation_date','action' )
+    search_fields = ('audited_user', 'description')
+    list_filter = (
+        ("creation_date", DateRangeQuickSelectListFilterBuilder()), ('action'),  # Range + QuickSelect Filter
+    )
 
 admin.site.register(Audit, AuditAdmin)
--- audit_app/models.py
+++ audit_app/models.py
@@ -5,9 +5,10 @@ from django.db import models
 class Audit(models.Model):
     id = models.AutoField(primary_key=True)
     source_app = models.CharField(max_length=30)
-    audited_user = models.CharField(max_length=30)
-    description = models.TextField()
-    creation_date = models.DateTimeField()
+    audited_user = models.CharField(max_length=30, verbose_name= "usuário auditado")
+    description = models.TextField(verbose_name="descrição")
+    creation_date = models.DateTimeField(verbose_name="data de criação")
+    action = models.CharField(max_length=30, verbose_name="ação")
 
     class Meta:
         managed = False
