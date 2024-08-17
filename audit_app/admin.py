8ee5b7c5c007b12e9579cdae0034be1480e642b8
T01764 <pedro.rodrigues@taking.com.br>
2024-08-17 17:16:57 -0300

    feat: fix code

--- audit_app/admin.py
+++ audit_app/admin.py
@@ -9,10 +9,15 @@ from audit_app.models import Audit
 # Register your models here.
 class AuditAdmin(admin.ModelAdmin):
     model = Audit
-    list_display = ('id', 'audited_user', 'description', 'creation_date','action' )
+    list_display = ('id', 'audited_user', 'description', 'creation_date', 'action')
     search_fields = ('audited_user', 'description')
     list_filter = (
         ("creation_date", DateRangeQuickSelectListFilterBuilder()), ('action'),  # Range + QuickSelect Filter
     )
+
+    def get_queryset(self, request):
+        queryset = super().get_queryset(request)
+        return queryset.filter(source_app='SISAQUA')
+
 
 admin.site.register(Audit, AuditAdmin)
